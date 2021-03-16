5abf90a0ba49aa99662ebeafcde4ab5e462333f9
Jairo Alberto <jairoalbertosalinasprimo210@gmail.com>
2021-03-15 23:03:57 -0500

    structuring the css well and adding javascript to the project

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-const btn_menu = document.querySelector('.btn-menu');
+document.addEventListener('DOMContentLoaded', _ => {
+    const btn_menu = document.querySelector('.navbar__btn-toggle');
+    const background = document.querySelector('.navbar__background');
+    const navbar = document.querySelector('.navbar');
 
-btn_menu.addEventListener('click', _ => {
-    const hero_links = document.querySelector('.hero_links')
-    hero_links.classList.toggle('open')
-})
+    btn_menu.addEventListener('click', _ => {
+        navbar.classList.add('navbar__sidebar--open')
+        console.log('a')
+    })
+    background.addEventListener('click', _ => {    
+        navbar.classList.remove('navbar__sidebar--open')
+        console.log('a')
+    });
+});
